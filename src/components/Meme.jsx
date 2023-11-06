a67ea107978b7c9f6373745f462e4bfd6b015e2f
Mengyuan Li <mli114@volvocars.com>
2023-11-06 20:06:37 +0100

    added meme images

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,19 +1,33 @@
+import memesData from "../memesData.js";
+import React from "react";
+
 export default function Meme() {
+	const [memeImage, setMemeImage] = React.useState("");
+	const memes = memesData.data.memes;
+	function getRandomMeme() {
+		const randomMeme = memes[Math.floor(Math.random() * memes.length)];
+		setMemeImage(randomMeme.url);
+	}
 	return (
-		<main className="meme">
-			<form className="form">
+		<main>
+			<div className="form">
 				<input
 					type="text"
+					placeholder="Top text"
 					className="form--input"
-					placeholder="Top Text"
 				/>
 				<input
 					type="text"
+					placeholder="Bottom text"
 					className="form--input"
-					placeholder="Bottom Text"
 				/>
-				<button className="form--button">Get a new meme image 🖼</button>
-			</form>
+				<button className="form--button" onClick={getRandomMeme}>
+					Get a new meme image 🖼
+				</button>
+			</div>
+			{memeImage && (
+				<img src={memeImage} alt="meme" className="meme--image" />
+			)}
 		</main>
 	);
 }
